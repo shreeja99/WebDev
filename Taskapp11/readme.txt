5a7338d73dec5e9a5214ffba2c88956e3aeebab9
shreeja99 <shreejamahale999@gmail.com>
2026-04-30 00:10:10 +0530

    web tech assgns

---
+++ Taskapp11/readme.txt
@@ -0,0 +1,3 @@
+npm init -y
+npm i express mongoose
+node server.js
--- Taskapp11/server.js
+++ Taskapp11/server.js
@@ -1,36 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://127.0.0.1:27017/tasks');
+app.use(express.json());
+app.use(express.static(__dirname)); // serve index.html
 
-// Model
-const Task = mongoose.model('Task', { name: String });
+mongoose.connect("mongodb://127.0.0.1:27017/taskdb")
+.then(() => console.log("DB connected"))
+.catch(err => console.log(err));
 
-// Serve HTML file
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+// Schema + Model (inside same file for simplicity)
+const Task = mongoose.model("Task", {
+    title: String
 });
 
-// CREATE
-app.post('/add', async (req, res) => {
-    await Task.create({ name: req.body.task });
-    res.redirect('/');
-});
-
-// READ
-app.get('/tasks', async (req, res) => {
+// GET tasks
+app.get("/tasks", async (req, res) => {
     const tasks = await Task.find();
-    res.send(tasks);
+    res.json(tasks);
 });
 
-// DELETE
-app.get('/delete/:id', async (req, res) => {
-    await Task.findByIdAndDelete(req.params.id);
-    res.redirect('/');
+// ADD task
+app.post("/tasks", async (req, res) => {
+    await Task.create({ title: req.body.title });
+    res.send("Added");
 });
 
 app.listen(3000, () => console.log("Running"));
